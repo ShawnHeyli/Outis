5265671118e7163f3892445357a13758aed9c0f3
Hilan Meyran <hilanmeyran@protonmail.com>
2022-06-24 21:20:18 +0200

    Added arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use chrono::{Datelike, NaiveDate, Utc};
 use clap::Parser;
-use std::env;
 
 #[derive(Parser)]
-struct Cli {}
+#[clap(author, version, about)]
+struct Cli {
+    /// Commit only on workdays
+    #[clap(short, long, value_parser, default_value_t = false)]
+    workdays: bool,
+
+    /// Maximum number of commits per day
+    #[clap(short = 'c', long = "commits", value_parser, default_value_t = 3)]
+    max_commits: u32,
+
+    /// Start date of the range to commit, if not specified current day is used
+    /// Format : YYYY-MM-DD
+    #[clap(long, value_parser = format_date_args, default_value_t = NaiveDate::from_ymd(Utc::today().year(), 1, 1))]
+    start_date: NaiveDate,
+
+    /// End date of the range to commit, if not specified current day is used
+    /// Format : YYYY-MM-DD
+    #[clap(long, value_parser = format_date_args, default_value_t = Utc::today().naive_utc())]
+    end_date: NaiveDate,
+}
 
 fn main() {}
