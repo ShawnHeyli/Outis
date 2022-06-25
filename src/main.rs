1eb434c29dc68653cec5c7cdaecfd03bbd6d14e9
Hilan Meyran <hilanmeyran@protonmail.com>
2022-06-25 13:30:31 +0200

    function create_commit

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,44 @@ struct Cli {
     path: PathBuf,
 }
 
+#[cfg(target_family = "windows")]
+fn create_commit(message: &str, date: &str, repo_path: &PathBuf) {
+    Command::new("cmd")
+        .args(&[
+            "/C",
+            "git",
+            "commit",
+            "--quiet",
+            "--allow-empty",
+            "-m",
+            message,
+            "--date",
+            date,
+        ])
+        .current_dir(repo_path)
+        .status()
+        .expect("failed to create a commit");
+}
+
+#[cfg(target_family = "unix")]
+fn create_commit(message: &str, date: &str, repo_path: &PathBuf) {
+    Command::new("sh")
+        .args(&[
+            "-c",
+            "git",
+            "commit",
+            "--quiet",
+            "--allow-empty",
+            "-m",
+            message,
+            "--date",
+            date,
+        ])
+        .current_dir(repo_path)
+        .status()
+        .expect("failed to create a commit")
+}
+
 fn main() {
     let args = Cli::parse();
     let repo = match Repository::init(args.path) {
